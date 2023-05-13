a346ac06f256ef87777af73173a0ac29fb865b42
boney <boneyantony30@gmail.com>
2023-05-14 00:45:53 +0530

    added react.js components

---
+++ src/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://kskogprjprkpqzjysgso.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtza29ncHJqcHJrcHF6anlzZ3NvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODI4NzQ0MDksImV4cCI6MTk5ODQ1MDQwOX0.9pK2tVs0F5jWRa435EHRgYIXDLP2Cm6Rn5huQ3lncdI";
+const supabase = createClient(supabaseUrl, supabaseKey);
+export default supabase;
---
+++ v1/README.md
@@ -0,0 +1,2 @@
+# today-i-learned
+A project I am doing as part of the Udemy course that I am currently undertaking/
